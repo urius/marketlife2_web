2939b1985f6aa1f52828e8ad1e2199329e4f0d44
yuryisupov <isup89@gmail.com>
2024-07-24 17:40:10 +0300

    LabeledArrayAttribute update logic (can display array elements)

--- Assets/Editor/LabeledArrayDrawer.cs
+++ Assets/Editor/LabeledArrayDrawer.cs
@@ -1,3 +1,4 @@
+using System;
 using Other;
 using UnityEditor;
 using UnityEngine;
@@ -20,7 +21,20 @@ namespace Editor
                 var fieldName = ((LabeledArrayAttribute)attribute).FieldName;
                 var field = property.boxedValue.GetType().GetField(fieldName);
                 var fieldValue = field.GetValue(property.boxedValue);
-                EditorGUI.PropertyField(rect, property, new GUIContent(fieldValue.ToString()), true);
+                if (fieldValue is Array fieldValueArray)
+                {
+                    var resultString = string.Empty;
+                    for (var i = 0; i < fieldValueArray.Length; i++)
+                    {
+                        resultString += (i == 0 ? string.Empty : ", ") + fieldValueArray.GetValue(i);
+                    }
+
+                    EditorGUI.PropertyField(rect, property, new GUIContent(resultString), true);
+                }
+                else
+                {
+                    EditorGUI.PropertyField(rect, property, new GUIContent(fieldValue.ToString()), true);
+                }
             }
             catch
             {
