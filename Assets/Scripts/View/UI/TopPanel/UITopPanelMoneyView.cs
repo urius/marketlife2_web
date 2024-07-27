3f14d0ed5d7e074b7bc65be04fd6c4eddf11a581
yuryisupov <isup89@gmail.com>
2024-07-27 19:52:08 +0300

    Insufficient money blink

--- Assets/Scripts/View/UI/TopPanel/UITopPanelMoneyView.cs
+++ Assets/Scripts/View/UI/TopPanel/UITopPanelMoneyView.cs
@@ -1,18 +1,22 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace View.UI.TopPanel
 {
     public class UITopPanelMoneyView : MonoBehaviour
     {
         [SerializeField] private TMP_Text _text;
+        [SerializeField] private RectTransform _moneyIconTransform;
         
         private Color _textDefaultColor;
+        private Vector2 _defaultMoneyIconPosition;
 
         public Color TextDefaultColor => _textDefaultColor;
 
         private void Awake()
         {
+            _defaultMoneyIconPosition = _moneyIconTransform.anchoredPosition;
             _textDefaultColor = _text.color;
         }
 
@@ -24,6 +28,21 @@ namespace View.UI.TopPanel
         public void SetTextColor(Color color)
         {
             _text.color = color;
+        }
+        
+        public void SetDefaultTextColor()
+        {
+            _text.color = _textDefaultColor;
+        }
+
+        public void SetMoneyIconXOffset(float xOffset)
+        {
+            _moneyIconTransform.anchoredPosition = new Vector2(xOffset, _defaultMoneyIconPosition.y);
+        }
+
+        public void ResetMoneyIconPosition()
+        {
+            _moneyIconTransform.anchoredPosition = _defaultMoneyIconPosition;
         }
     }
 }
--- Assets/Scripts/View/UI/TopPanel/UITopPanelMoneyViewMediator.cs
+++ Assets/Scripts/View/UI/TopPanel/UITopPanelMoneyViewMediator.cs
@@ -4,6 +4,7 @@ using Infra.Instance;
 using Model;
 using UnityEngine;
 using Utils;
+using Random = UnityEngine.Random;
 
 namespace View.UI.TopPanel
 {
@@ -13,10 +14,16 @@ namespace View.UI.TopPanel
         private readonly IUpdatesProvider _updatesProvider = Instance.Get<IUpdatesProvider>();
 
         private readonly MoneyAnimationContext _moneyAnimationContext = new ();
+        private readonly Color[] _blinkColors = { Color.white, Color.red };
         
         private UITopPanelMoneyView _moneyView;
         private PlayerModel _playerModel;
         private int _currentMoneyDisplayed;
+        private int _blinksCounter;
+        private int _blinkMoneyCooldown;
+        private int _currentColorIndex;
+        private float _moneyIconXPositionOffsetPercent = 0.5f;
+        private int _moneyIconXPositionDirection = 0;
 
         protected override void MediateInternal()
         {
@@ -39,13 +46,58 @@ namespace View.UI.TopPanel
         private void Subscribe()
         {
             _playerModel.MoneyChanged += OnMoneyChanged;
+            _playerModel.InsufficientFunds += OnInsufficientFundsSignal;
         }
 
         private void Unsubscribe()
         {
             _playerModel.MoneyChanged -= OnMoneyChanged;
+            _playerModel.InsufficientFunds -= OnInsufficientFundsSignal;
             
             _updatesProvider.GameplayFixedUpdate -= OnMoneyAnimationGameplayFixedUpdate;
+        }
+
+        private void OnInsufficientFundsSignal(int _)
+        {
+            _updatesProvider.GameplayFixedUpdate -= OnBlinkMoneyGameplayFixedUpdate;
+            _updatesProvider.GameplayFixedUpdate += OnBlinkMoneyGameplayFixedUpdate;
+            
+            _blinksCounter = 0;
+            _currentColorIndex = 0;
+            _blinkMoneyCooldown = 0;
+            _moneyIconXPositionOffsetPercent = 0.5f;
+            _moneyIconXPositionDirection = Random.value <= 0.5f ? 1 : -1;
+        }
+
+        private void OnBlinkMoneyGameplayFixedUpdate()
+        {
+            _blinkMoneyCooldown--;
+
+            _moneyIconXPositionOffsetPercent += Time.fixedDeltaTime * _moneyIconXPositionDirection * 10;
+            var offset = Mathf.Lerp(-5, 5, _moneyIconXPositionOffsetPercent);
+            _moneyView.SetMoneyIconXOffset(offset);
+
+            if ((_moneyIconXPositionOffsetPercent > 1 && _moneyIconXPositionDirection > 0)
+                || (_moneyIconXPositionOffsetPercent <= 0 && _moneyIconXPositionDirection < 0))
+            {
+                _moneyIconXPositionDirection *= -1;
+            }
+
+            if (_blinkMoneyCooldown <= 0)
+            {
+                _blinksCounter++;
+                _blinkMoneyCooldown = 5;
+                _currentColorIndex = 1 - _currentColorIndex;
+
+                _moneyView.SetTextColor(_blinkColors[_currentColorIndex]);
+            }
+
+            if (_blinksCounter >= 10)
+            {
+                _moneyView.SetDefaultTextColor();
+                _moneyView.ResetMoneyIconPosition();
+                _updatesProvider.GameplayFixedUpdate -= OnBlinkMoneyGameplayFixedUpdate;
+            }
         }
 
         private void OnMoneyChanged(int _)
